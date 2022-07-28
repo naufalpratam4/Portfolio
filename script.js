77aa33082492e7127ac1ca7b91882ca40676bdc9
naufalpratam4 <98386574+naufalpratam4@users.noreply.github.com>
2022-07-28 16:48:57 +0700

    Add files via upload
    Membuat website menggunakan bootsrap

---
+++ script.js
@@ -0,0 +1,11 @@
+const viewFilm = document.querySelector("#viewFilm");
+
+viewFilm.addEventListener("click", function () {
+  alert("Web Film Sedang dalam perbaikan");
+});
+
+const whatsapp = document.querySelector(".WhatsApp");
+
+whatsapp.addEventListener("click", function () {
+  alert("WhatsApp saya hanya untuk pribadi, DM Instagram aja yaa 😍");
+});
